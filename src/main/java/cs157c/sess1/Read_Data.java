880c02d76314bea743051137dfd120d3340d594a
younmeeoh <chewybottom@gmail.com>
2021-04-19 15:18:12 -0700

    initial commit for first code review

---
+++ src/main/java/cs157c/sess1/Read_Data.java
@@ -0,0 +1,17 @@
+package cs157c.sess1;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+public class Read_Data{
+    public static void main(String args[])throws Exception{
+        //queries 
+        String query = "SELECT * FROM emp";
+        //Creating Cluster object 
+        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        //Creating Session object 
+        Session session = cluster.connect("oh");
+        ResultSet result = session.execute(query);
+        System.out.println(result.all());
+    }
+}
