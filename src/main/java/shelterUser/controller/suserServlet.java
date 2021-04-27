bd185752c17923fd10f519ef5aa4222068e6fc11
younmeeoh <chewybottom@gmail.com>
2021-04-27 15:26:25 -0700

    sign up page new workspace

---
+++ src/main/java/shelterUser/controller/suserServlet.java
@@ -0,0 +1,73 @@
+package shelterUser.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import shelterUser.suserDAO.s_user_dao;
+import shelterUser.suserModel.shelterUser;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/register")
+public class suserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private s_user_dao suser_dao = new s_user_dao();
+	
+	public void init() {
+		suser_dao = new s_user_dao();
+	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public suserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/adoptable.web/account.jsp");
+		dispatcher.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+
+		
+		shelterUser suser = new shelterUser();
+		suser.setUsername(username);
+		suser.setPassword(password);
+		suser.setEmail(email);
+		suser.setAddress(address);
+		
+		try {
+			s_user_dao.registerSuser(suser);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("account.jsp");
+		dispatcher.forward(request,response);
+	}
+
+}
---
+++ src/main/java/shelterUser/suserDAO/s_user_dao.java
@@ -0,0 +1,29 @@
+package shelterUser.suserDAO;
+
+import com.datastax.driver.core.exceptions.DriverException;
+
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.BoundStatement;
+import shelterUser.suserModel.shelterUser;
+
+public class s_user_dao{
+    public static void registerSuser(shelterUser suser) throws DriverException{   	
+        //queries 
+        String insert_user = "INSERT INTO shelterusers (userid,username, address, email, password) values (12,?,?,?,?)";
+        //Creating Cluster object 
+        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        //Creating Session object 
+        Session session = cluster.connect("sfapp");
+  
+        PreparedStatement prepared = session.prepare(insert_user);
+    	BoundStatement bound = prepared.bind(suser.getUsername(),suser.getAddress(),suser.getEmail(),suser.getPassword());
+    	ResultSet result = session.execute(bound);    	
+    	System.out.println(result.all());
+        
+    
+    }
+}
