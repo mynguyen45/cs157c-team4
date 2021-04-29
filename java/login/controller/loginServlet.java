50aadf18e5430775cf7fc1e9188edd984f45cc6e
My Nguyen <55858280+mynguyen45@users.noreply.github.com>
2021-04-29 15:52:26 -0700

    Add files via upload

---
+++ java/login/controller/loginServlet.java
@@ -0,0 +1,69 @@
+package login.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import regularUser.ruserDAO.r_user_dao;
+import regularUser.ruserModel.regularUser;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/login")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	//private r_user_dao suser_dao = new r_user_dao();
+	
+	//public void init() {
+	//	suser_dao = new r_user_dao();
+	//}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/MavenWebApp/login.jsp");
+		dispatcher.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		
+		regularUser suser = new regularUser();
+		suser.setUsername(username);
+		suser.setPassword(password);
+		
+		try {
+			r_user_dao.registerRuser(suser);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request,response);
+	}
+
+}
